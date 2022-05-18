83d539abac42e3933c82e3b4aa84cb393b5fea51
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-05-18 19:09:34 +0200

    Add iteration

--- learning-git-task/lista_zakupów.py
+++ learning-git-task/lista_zakupów.py
@@ -3,4 +3,6 @@ shopping_list = {
     'warzywniak': ['pomidory', 'cebula', 'ogórek'],
     'stacja benzynowa': ['benzyna', 'kawa']
     }
-    
+
+for k, v in shopping_list.items():
+    print(f"Idę do {k.capitalize()} i kupuję tam {[item.capitalize() for item in v]}")
