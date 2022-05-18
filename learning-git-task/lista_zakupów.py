ed02504240a3cea5b5efbb318ea8bc7aabf87754
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-05-18 19:19:44 +0200

    Add items counting

--- learning-git-task/lista_zakupów.py
+++ learning-git-task/lista_zakupów.py
@@ -4,5 +4,10 @@ shopping_list = {
     'stacja benzynowa': ['benzyna', 'kawa']
     }
 
+number_of_items = 0
+
 for k, v in shopping_list.items():
     print(f"Idę do {k.capitalize()} i kupuję tam {[item.capitalize() for item in v]}")
+    number_of_items += len(v)
+
+print(f"W sumie kupuję {number_of_items} produktów.")
